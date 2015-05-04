24c01fed7f9b87a1fe97c7cb90deda28ba39399d
Nam Hoang <nam.xuan.hoang.work@gmail.com>
2015-05-04 11:47:38 -0600

    Test providing Tag view

--- rest/urls.py
+++ rest/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from rest import views
 
-
-
+urlpatterns = [
+    url(r'^tags/$', views.TagList.as_view()),
+]
--- rest/views.py
+++ rest/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest.serializers import TagSerializer
+from rest_framework.response import Response
+from rest.models import Tag
 
-# Create your views here.
+class TagList(APIView):
+   def get(self, request):
+        tags = Tag.objects.all()
+        serializer = TagSerializer(tags, many=True)
+        return Response(serializer.data)
