737442423981976fd533922011e9bf7eb5ca4f40
Nam Hoang <nam.xuan.hoang.work@gmail.com>
2015-11-22 10:59:30 +0700

    Put for Contact

--- rest/views.py
+++ rest/views.py
@@ -15,7 +15,7 @@ class ContactDetail(APIView):
         except Contact.DoesNotExist:
             return None
 
-    def get(self, request, pk, format=None):
+    def get(self, request, pk):
         contact = self.get_object(pk)
         if contact is not None:
             serializer = ContactSerializer(contact)
@@ -23,4 +23,30 @@ class ContactDetail(APIView):
         else:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
-    # def put(self, request, pk, format=None):
+    def put(self, request, pk):
+        server_contact = self.get_object(pk)
+
+        if server_contact is not None:
+            serializer = ContactSerializer(server_contact, data=request.data)
+
+            if serializer.is_valid():
+                client_contact = Contact(**serializer.validated_data)
+
+                if client_contact.LastModified > server_contact.LastModified:
+                    if server_contact.IsDeleted:
+                        return Response(status=status.HTTP_410_GONE)
+                    else:
+                        # serializer = ContactSerializer(contact, data=request.data)
+                        # if serializer.is_valid():
+                        #     serializer.save()
+                        # else:
+                        #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                        serializer.save()
+                        return Response(status=status.HTTP_204_NO_CONTENT)
+                else:
+                    server_serializer = ContactSerializer(server_contact)
+                    return Response(server_serializer.data, status=status.HTTP_409_CONFLICT)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(status=status.HTTP_410_GONE)
