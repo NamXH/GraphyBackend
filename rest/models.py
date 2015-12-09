c29bd2718244afc013a16ffd17abd10203195e22
Nam Hoang <nam.xuan.hoang.work@gmail.com>
2015-12-09 22:21:59 +0700

    Add serializers

--- rest/models.py
+++ rest/models.py
@@ -22,85 +22,85 @@ class Tag(models.Model):
     IsDeleted = models.BooleanField(default=False)
 
 
-# class RelationshipType(models.Model):
-#     Id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-#     Name = models.CharField(max_length=255, null=True, blank=True)
-#     LastModified = models.DateTimeField(null=True, blank=True)
-#     IsDeleted = models.BooleanField(default=False)
-#
-#
-# class ContactTagMap(models.Model):
-#     Id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     Detail = models.CharField(max_length=255, null=True, blank=True)
-#     ContactId = models.ForeignKey(Contact, null=True, blank=True)
-#     TagId = models.ForeignKey(Tag, null=True, blank=True)
-#     LastModified = models.DateTimeField(null=True, blank=True)
-#     IsDeleted = models.BooleanField(default=False)
-#
-#
-# class Relationship(models.Model):
-#     Id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     Detail = models.CharField(max_length=255, null=True, blank=True)
-#     FromContactId = models.ForeignKey(Contact, null=True, blank=True)
-#     ToContactId = models.ForeignKey(Contact, null=True, blank=True)
-#     RelationshipTypeId = models.ForeignKey(RelationshipType, null=True, blank=True)
-#     LastModified = models.DateTimeField(null=True, blank=True)
-#     IsDeleted = models.BooleanField(default=False)
-#
-#
-# class PhoneNumber(models.Model):
-#     Id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     Type = models.CharField(max_length=255, null=True, blank=True)
-#     Number = models.CharField(max_length=255, null=True, blank=True)
-#     ContactId = models.ForeignKey(Contact, null=True, blank=True)
-#     LastModified = models.DateTimeField(null=True, blank=True)
-#     IsDeleted = models.BooleanField(default=False)
-#
-#
-# class Address(models.Model):
-#     Id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     Type = models.CharField(max_length=255, null=True, blank=True)
-#     StreetLine1 = models.CharField(max_length=255, null=True, blank=True)
-#     StreetLine2 = models.CharField(max_length=255, null=True, blank=True)
-#     City = models.CharField(max_length=255, null=True, blank=True)
-#     Province = models.CharField(max_length=255, null=True, blank=True)
-#     PostalCode = models.CharField(max_length=255, null=True, blank=True)
-#     Country = models.CharField(max_length=255, null=True, blank=True)
-#     ContactId = models.ForeignKey(Contact, null=True, blank=True)
-#     LastModified = models.DateTimeField(null=True, blank=True)
-#     IsDeleted = models.BooleanField(default=False)
-#
-#
-# class Url(models.Model):
-#     Id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     Type = models.CharField(max_length=255, null=True, blank=True)
-#     Link = models.CharField(max_length=255, null=True, blank=True)
-#     LastModified = models.DateTimeField(null=True, blank=True)
-#     IsDeleted = models.BooleanField(default=False)
-#
-#
-# class Email(models.Model):
-#     Id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     Type = models.CharField(max_length=255, null=True, blank=True)
-#     Address = models.CharField(max_length=255, null=True, blank=True)
-#     ContactId = models.ForeignKey(Contact, null=True, blank=True)
-#     LastModified = models.DateTimeField(null=True, blank=True)
-#     IsDeleted = models.BooleanField(default=False)
-#
-#
-# class SpecialDate(models.Model):
-#     Id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     Type = models.CharField(max_length=255, null=True, blank=True)
-#     Date = models.DateField(null=True, blank=True)
-#     ContactId = models.ForeignKey(Contact, null=True, blank=True)
-#     LastModified = models.DateTimeField(null=True, blank=True)
-#     IsDeleted = models.BooleanField(default=False)
-#
-#
-# class InstantMessage(models.Model):
-#     Id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     Type = models.CharField(max_length=255, null=True, blank=True)
-#     Nickname = models.CharField(max_length=255, null=True, blank=True)
-#     ContactId = models.ForeignKey(Contact, null=True, blank=True)
-#     LastModified = models.DateTimeField(null=True, blank=True)
-#     IsDeleted = models.BooleanField(default=False)
+class RelationshipType(models.Model):
+    Id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    Name = models.CharField(max_length=255, null=True, blank=True)
+    LastModified = models.DateTimeField(null=True, blank=True)
+    IsDeleted = models.BooleanField(default=False)
+
+
+class ContactTagMap(models.Model):
+    Id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    Detail = models.CharField(max_length=255, null=True, blank=True)
+    ContactId = models.ForeignKey(Contact, null=True, blank=True)
+    TagId = models.ForeignKey(Tag, null=True, blank=True)
+    LastModified = models.DateTimeField(null=True, blank=True)
+    IsDeleted = models.BooleanField(default=False)
+
+
+class Relationship(models.Model):
+    Id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    Detail = models.CharField(max_length=255, null=True, blank=True)
+    FromContactId = models.ForeignKey(Contact, null=True, blank=True)
+    ToContactId = models.ForeignKey(Contact, null=True, blank=True)
+    RelationshipTypeId = models.ForeignKey(RelationshipType, null=True, blank=True)
+    LastModified = models.DateTimeField(null=True, blank=True)
+    IsDeleted = models.BooleanField(default=False)
+
+
+class PhoneNumber(models.Model):
+    Id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    Type = models.CharField(max_length=255, null=True, blank=True)
+    Number = models.CharField(max_length=255, null=True, blank=True)
+    ContactId = models.ForeignKey(Contact, null=True, blank=True)
+    LastModified = models.DateTimeField(null=True, blank=True)
+    IsDeleted = models.BooleanField(default=False)
+
+
+class Address(models.Model):
+    Id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    Type = models.CharField(max_length=255, null=True, blank=True)
+    StreetLine1 = models.CharField(max_length=255, null=True, blank=True)
+    StreetLine2 = models.CharField(max_length=255, null=True, blank=True)
+    City = models.CharField(max_length=255, null=True, blank=True)
+    Province = models.CharField(max_length=255, null=True, blank=True)
+    PostalCode = models.CharField(max_length=255, null=True, blank=True)
+    Country = models.CharField(max_length=255, null=True, blank=True)
+    ContactId = models.ForeignKey(Contact, null=True, blank=True)
+    LastModified = models.DateTimeField(null=True, blank=True)
+    IsDeleted = models.BooleanField(default=False)
+
+
+class Url(models.Model):
+    Id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    Type = models.CharField(max_length=255, null=True, blank=True)
+    Link = models.CharField(max_length=255, null=True, blank=True)
+    LastModified = models.DateTimeField(null=True, blank=True)
+    IsDeleted = models.BooleanField(default=False)
+
+
+class Email(models.Model):
+    Id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    Type = models.CharField(max_length=255, null=True, blank=True)
+    Address = models.CharField(max_length=255, null=True, blank=True)
+    ContactId = models.ForeignKey(Contact, null=True, blank=True)
+    LastModified = models.DateTimeField(null=True, blank=True)
+    IsDeleted = models.BooleanField(default=False)
+
+
+class SpecialDate(models.Model):
+    Id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    Type = models.CharField(max_length=255, null=True, blank=True)
+    Date = models.DateField(null=True, blank=True)
+    ContactId = models.ForeignKey(Contact, null=True, blank=True)
+    LastModified = models.DateTimeField(null=True, blank=True)
+    IsDeleted = models.BooleanField(default=False)
+
+
+class InstantMessage(models.Model):
+    Id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    Type = models.CharField(max_length=255, null=True, blank=True)
+    Nickname = models.CharField(max_length=255, null=True, blank=True)
+    ContactId = models.ForeignKey(Contact, null=True, blank=True)
+    LastModified = models.DateTimeField(null=True, blank=True)
+    IsDeleted = models.BooleanField(default=False)
--- rest/serializers.py
+++ rest/serializers.py
@@ -11,16 +11,58 @@ class TagSerializer(serializers.ModelSerializer):
     Id = serializers.UUIDField()
     class Meta:
         model = Tag
-        fields = ('Id', 'Name', 'IsDeleted', 'LastModified')
-#
-# class PhoneNumberSerializer(serializers.ModelSerializer):
-#         Id = serializers.UUIDField()
-#     class Meta:
-#         model = PhoneNumber
-#         fields = ('id', 'type', 'number')
-#
-# class EmailSerializer(serializers.ModelSerializer):
-#         Id = serializers.UUIDField()
-#     class Meta:
-#         model = Email
-#         fields = ('id', 'type', 'street_line_1')
+        fields = ('Id', 'Name', 'LastModified', 'IsDeleted')
+
+class RelationshipTypeSerializer(serializers.ModelSerializer):
+    Id = serializers.UUIDField()
+    class Meta:
+        model = RelationshipType
+        fields = ('Id', 'Name', 'LastModified', 'IsDeleted')
+
+class ContactTagMapSerializer(serializers.ModelSerializer):
+    Id = serializers.UUIDField()
+    class Meta:
+        model = ContactTagMap
+        fields = ('Id', 'Detail', 'ContactId', 'TagId', 'LastModified', 'IsDeleted')
+
+class RelationshipSerializer(serializers.ModelSerializer):
+    Id = serializers.UUIDField()
+    class Meta:
+        model = Relationship
+        fields = ('Id', 'Detail', 'FromContactId', 'ToContactId', 'RelationshipTypeId', 'LastModified', 'IsDeleted')
+
+class PhoneNumberSerializer(serializers.ModelSerializer):
+    Id = serializers.UUIDField()
+    class Meta:
+        model = PhoneNumber
+        fields = ('Id', 'Type', 'Number', 'ContactId', 'LastModified', 'IsDeleted')
+
+class AddressSerializer(serializers.ModelSerializer):
+    Id = serializers.UUIDField()
+    class Meta:
+        model = Address
+        fields = ('Id', 'Type', 'StreetLine1', 'StreetLine2', 'City', 'Province', 'PostalCode', 'Country', 'ContactId', 'LastModified', 'IsDeleted')
+
+class UrlSerializer(serializers.ModelSerializer):
+    Id = serializers.UUIDField()
+    class Meta:
+        model = Url
+        fields = ('Id', 'Type', 'Link', 'LastModified', 'IsDeleted')
+
+class EmailSerializer(serializers.ModelSerializer):
+    Id = serializers.UUIDField()
+    class Meta:
+        model = Email
+        fields = ('Id', 'Type', 'Address', 'ContactId', 'LastModified', 'IsDeleted')
+
+class SpecialDateSerializer(serializers.ModelSerializer):
+    Id = serializers.UUIDField()
+    class Meta:
+        model = SpecialDate
+        fields = ('Id', 'Type', 'Date', 'ContactId', 'LastModified', 'IsDeleted')
+
+class InstantMessageSerializer(serializers.ModelSerializer):
+    Id = serializers.UUIDField()
+    class Meta:
+        model = InstantMessage
+        fields = ('Id', 'Type', 'Nickname', 'ContactId', 'LastModified', 'IsDeleted')
